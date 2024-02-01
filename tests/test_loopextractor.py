c6e1f1a6843437db0a3847faed8e05d8a9a236a5
Jordan Smith <jblsmith@gmail.com>
2024-02-01 21:08:35 +0000

    Added a couple basic tests. Moved test song

---
+++ tests/test_loopextractor.py
@@ -0,0 +1,7 @@
+from loopextractor.loopextractor import get_recommended_template_sizes
+
+import numpy as np
+
+def test_get_recommended_template_sizes():
+    recommended_size = get_recommended_template_sizes(np.zeros((10,100,1000)))
+    assert np.all(recommended_size == [3,15,63])
