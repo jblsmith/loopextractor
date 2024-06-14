9e67318effadd1fda78c7c800c0b5366e0196e4f
Jordan Bennett Louis Smith <eex813@cln-jfr-cm4qjny7gh.home>
2024-06-14 17:38:33 +0100

    Added test of purification function

--- tests/test_loopextractor.py
+++ tests/test_loopextractor.py
@@ -1,9 +1,24 @@
-from loopextractor.loopextractor import get_recommended_template_sizes
+from loopextractor.loopextractor import get_recommended_template_sizes, purify_core_tensor
 
 import numpy as np
 
 def test_get_recommended_template_sizes():
     recommended_size = get_recommended_template_sizes(np.zeros((10,100,1000)))
     assert np.all(recommended_size == [3,15,63])
+
+
+def test_purify_cube():
+    thing1 = np.random.randint(0, 1000, (5,5))
+    thing2 = np.random.randint(0, 1000, (5,5))
+    thing3 = np.random.randint(0, 1000, (5,5))
+    thing4 = thing1 + 2*thing2
+    factors = [[], [], np.array([[1,0], [1,0], [0,1], [0,1]]).T]
+    core_tensor = np.dstack([thing1, thing2, thing3, thing4])
+    pure_tensor, pure_factors = purify_core_tensor(core_tensor, factors, new_rank=3, dim_to_reduce=2)
+    assert pure_tensor.shape == (5,5,3)
+
+    bar_0_before = np.dot(core_tensor, factors[2][0, :])
+    bar_0_after = np.dot(pure_tensor, pure_factors[2][0, :])
+    assert np.all(np.abs(bar_0_before - bar_0_after) < bar_0_before)
 
 # FIXME: Add more tests
